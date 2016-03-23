ec34fe8d60a14bd0738a3c01a1668dcb22e1e71f
turtleDev <saravjeetamansingh@gmail.com>
2016-03-23 17:39:29 +0530

    add minimal build system

--- build.js
+++ build.js
@@ -1,1 +1,49 @@
-console.log('I run');
+'use strict';
+
+var hbs = require("handlebars");
+var fs = require("fs");
+var async = require("async");
+var path = require("path");
+
+var inFile = "./src/scramble.js.hbs";
+var outFile = "./dist/scramble.js";
+
+var ent = "./entities.json";
+var chars = "./chars.txt";
+
+async.map([inFile, ent, chars], fs.readFile, function(err, data) {
+    if ( err ) {
+        console.error(err);
+        process.exit(0);
+    }
+
+    var raw = data[0].toString();
+    var ent = data[1].toString();
+    var chars = data[2].toString().split('');
+
+    ent = JSON.parse(ent);
+
+    chars = chars.map(function(char) {
+        return ent[char] || char;
+    });
+    debugger;
+    
+    var template = hbs.compile(raw, {noEscape: true});
+    var output = template({symbols:chars});
+
+    try {
+        fs.mkdirSync(path.dirname(outFile))
+    } catch(e) {
+        if ( e.code != 'EEXIST' )
+            throw(e);
+    }
+
+    fs.writeFile(outFile, output, function(err) {
+        if ( err ) {
+            console.error(err);
+        }
+        console.log("build success!");
+        console.log(outFile);
+    });
+});
+
